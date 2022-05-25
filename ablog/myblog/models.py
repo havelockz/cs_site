fa039e421e7315eaadbaa1b98970958b0a845a3e
havelockz <paradox.vr@gmail.com>
2022-05-25 15:16:31 +0300

    created blog like button

--- ablog/myblog/models.py
+++ ablog/myblog/models.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="ЦКЗ НГУ")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_posts')
+
+    def total_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- ablog/myblog/views.py
+++ ablog/myblog/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.http import HttpResponseRedirect
 
 
 # def home(request):
 #    return render(request, 'home.html', {})
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
-    #ordering = ['-id']
+    # ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data()
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context["total_likes"] = total_likes
+        return context
 
 
 class AddPostView(CreateView):
