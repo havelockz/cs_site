7c8d61b56858e7aec740d6b717a8298d40e5dfca
havelockz <paradox.vr@gmail.com>
2022-05-25 17:05:48 +0300

    created blog unlike button

--- ablog/myblog/views.py
+++ ablog/myblog/views.py
@@ -10,7 +10,13 @@ from django.http import HttpResponseRedirect
 #    return render(request, 'home.html', {})
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -29,7 +35,14 @@ class ArticleDetailView(DetailView):
         context = super(ArticleDetailView, self).get_context_data()
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
         context["total_likes"] = total_likes
+        context["liked"] = liked
+
         return context
 
 
